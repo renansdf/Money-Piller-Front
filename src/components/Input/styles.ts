853d9bfaa71a12a4de6f8fffe152c2b05f264413
renansdf <rnn.frts@gmail.com>
2020-10-30 19:49:58 -0300

    added styles for input

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledInput = styled.input`
+  border: none;
+  color: #3405A2;
+  font-size: 16px;
+  padding: 4px 14px;
+  border-radius: 3px;
+  text-align: center;
+  box-sizing: border-box;
+
+  &:hover, &:focus{
+    outline: none;
+  }
+`;
